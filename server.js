75a1f4a9b12cd523353c9d3696ce7c8db312f5c0
Raghu M N <raghumn688@gmail.com>
2022-01-11 00:33:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+http = require('http')
+const fs = require('fs')
+const url = require('url') 
+const PORT = process.evn.PORT||5000 
+http.createServer(function(req,res){
+    var q = url.parse(req.url,true) //taking the url request storing in q checking the format by printing q 
+    // console.log(q)
+    //q.pathname = /filename if you pass localhost:5000/index.html  
+    var filename = "." +q.pathname 
+    if (filename =="./"){
+        filename = './index.html'
+    }
+    fs.readFile(filename,(err,data)=>{
+        if(err){
+            res.writeHead(404,{'content-Type':'text/html'})
+            res.write('404 Not found')
+            return res.end();
+        } 
+        res.writeHead(200,{'content-Type':'text/html'})
+        console.log(req.url) //printing request url
+        res.write(data)
+        return res.end()
+    })
+}).listen(5000)
+console.log('Server listening on 5000..... ')
